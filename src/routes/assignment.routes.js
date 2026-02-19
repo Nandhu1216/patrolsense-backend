39007984d2e3fe9dc15b90c78bd1378bd0323992
Nandhu1216 <srinandhu1216@gmail.com>
2026-02-19 14:12:37 +0530

    commit

--- src/routes/assignment.routes.js
+++ src/routes/assignment.routes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-
+const assignmentCtrl = require("../controllers/assignmentCtrl");
 const {
   getAssignments,
   createAssignment,
